316e14f40e8d2c01df5824814a85d39b5ca35f6a
Nisitha <wick.nisitha@gmail.com>
2025-03-11 23:28:04 +0530

    built file architecture for src, built flexbetween component and created a navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -13,6 +14,7 @@ function App() {
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
               <Routes>
                 <Route path="/" element={<div>dashboard page</div>} />
                 <Route
